395c1e7dc61c030992a9e685bd2966df49dc26ac
Tetsuya Yamamoto <tetsuya.x.yamamoto@sony.com>
2026-09-17 16:08:27 +0900

    Add CONV_2D operation to the micro_op_resolver
    To handle convolutional layers in the model, that was created by the user,
     the CONV_2D operation was added to the micro_op_resolver.

--- examples/micro_speech/micro_speech.ino
+++ examples/micro_speech/micro_speech.ino
@@ -72,7 +72,10 @@ void setup() {
   //
   // tflite::AllOpsResolver resolver;
   // NOLINTNEXTLINE(runtime-global-variables)
-  static tflite::MicroMutableOpResolver<4> micro_op_resolver;
+  static tflite::MicroMutableOpResolver<5> micro_op_resolver;
+  if (micro_op_resolver.AddConv2D() != kTfLiteOk) {
+    return;
+  }
   if (micro_op_resolver.AddDepthwiseConv2D() != kTfLiteOk) {
     return;
   }
